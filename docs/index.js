99199cbf2bb88f0c4eda424273bf3dc2cd60285a
Stuart A. Roth <stuartaroth@gmail.com>
2017-04-06 18:01:47 -0600

    Added full links

--- docs/index.js
+++ docs/index.js
@@ -264,13 +264,13 @@ var CodeService = (function () {
             { folder: Folder.WebServer, text: "Web Server" }
         ];
         this.getCodeLeft = function (language, folder) {
-            $.get("node_modules/programmersguidetothegalaxy/" + _this.getFolder(folder) + "/" + _this.getLanguage(language), function (data) {
+            $.get("http://stuartaroth.github.io/programmersguidetothegalaxy-site-jquery/node_modules/programmersguidetothegalaxy/" + _this.getFolder(folder) + "/" + _this.getLanguage(language), function (data) {
                 _this.currentCode = data;
                 _this.htmlService.updateCodeExampleLeft(language, data);
             });
         };
         this.getCodeRight = function (language, folder) {
-            $.get("node_modules/programmersguidetothegalaxy/" + _this.getFolder(folder) + "/" + _this.getLanguage(language), function (data) {
+            $.get("http://stuartaroth.github.io/programmersguidetothegalaxy-site-jquery/node_modules/programmersguidetothegalaxy/" + _this.getFolder(folder) + "/" + _this.getLanguage(language), function (data) {
                 _this.currentCode = data;
                 _this.htmlService.updateCodeExampleRight(language, data);
             });
